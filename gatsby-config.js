b61dd1d19f63e8e7a41469214249658f16871141
Dan Stelljes <dan@stellj.es>
2020-06-30 08:36:04 -0500

    Use Gatsby plugin to handle S3 deployment

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,14 @@ module.exports = {
       component: resolve('./src/layouts/default.js')
     }
   }, {
+    resolve: 'gatsby-plugin-s3',
+    options: {
+      bucketName: 'stellj.es',
+      hostname: 'stellj.es',
+      protocol: 'https',
+      removeNonexistentObjects: true
+    }
+  }, {
     resolve: 'gatsby-plugin-sass'
   }]
 }
